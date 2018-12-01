e37c9f1869e9d354ac40b26ca8c7f114e02ea62c
Jose Jara <joseyjara@gmail.com>
2018-12-01 07:51:45 -0700

    added route for the todolist component

---
+++ TodoList/ClientApp/src/app/Todo/todo-list.component.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var TodoList = /** @class */ (function () {
+    function TodoList() {
+    }
+    return TodoList;
+}());
+exports.TodoList = TodoList;
+//# sourceMappingURL=todo-list.component.js.map
